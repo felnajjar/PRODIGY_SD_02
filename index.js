b1c754254e7caeeaf5426ac3e3ab99568cc80726
FatimaAl-najjar <125657503+FatimaAl-najjar@users.noreply.github.com>
2024-02-04 23:57:12 +0200

    giu

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-const readline = require('node:readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
 var computerNum = parseInt(Math.random() * 100);
 var userNum;
-function getNumber() {
-    readline.question(`[Enter the number you want to guess] `, ans => {
-        userNum = parseInt(ans);
-        if (userNum > computerNum) {
-            console.log(`-> Go smaller `);
-            getNumber();
-        }
-        else if (userNum < computerNum) {
-            console.log(`-> Go higher `);
-            getNumber();
-        }
-        else {
-            console.log(`-> The answer you guessed is correct :) `);
-            readline.close();
-        }
-    });   
+var tries = 0;
+function getNumber(form) {
+    tries++;
+    userNum = parseInt(form.number.value);
+    if (userNum > computerNum) {
+        document.getElementById("result").innerHTML = `Go smaller! (${tries} tries)`;
+    }
+    else if (userNum < computerNum) {
+        document.getElementById("result").innerHTML = `Go higher! (${tries} tries)`;
+    }
+    else {
+        document.getElementById("result").innerHTML = `The answer you guessed is correct (${tries} tries)`;
+    }
 }
-getNumber()
